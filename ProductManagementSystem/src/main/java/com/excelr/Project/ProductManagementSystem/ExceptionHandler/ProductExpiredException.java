9ed4d6dea47ece6bb55abc46fb23155af977024e
manohar-srum <142774153+manohar-srum@users.noreply.github.com>
2024-01-01 11:34:12 +0530

    Delete ProductManagementSystem directory

--- ProductManagementSystem/src/main/java/com/excelr/Project/ProductManagementSystem/ExceptionHandler/ProductExpiredException.java
+++
@@ -1,8 +0,0 @@
-package com.excelr.Project.ProductManagementSystem.ExceptionHandler;
-
-public class ProductExpiredException extends Exception {
-    public ProductExpiredException(String message) {
-        super(message);
-    }
-}
-
